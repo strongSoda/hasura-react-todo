8e0faeec4a8b527ac32c419fb62a421978d8d57c
strongSoda <strongsoda2@gmail.com>
2018-12-27 11:09:25 +0530

    config

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,5 +1,7 @@
 export const AUTH_CONFIG = {
   domain: 'devhoot.auth0.com',
   clientId: 'hGk4Hok8fE9TslQbzTWJrnD2pe3Qi6IL',
+  responseType: 'token id_token',
+  scope: 'openid',
   callbackUrl: 'https://hasuratodowithapollo.herokuapp.com/callback'
 }
