9203d43420edfdbc06d32e0815151f15374897a5
strongSoda <strongsoda2@gmail.com>
2019-01-06 15:22:57 +0530

    checking on local host

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -3,5 +3,5 @@ export const AUTH_CONFIG = {
   clientId: 'hGk4Hok8fE9TslQbzTWJrnD2pe3Qi6IL',
   responseType: 'token id_token',
   scope: 'openid',
-  callbackUrl: 'https://hasuratodowithapollo.herokuapp.com/callback'
+  callbackUrl: 'http://localhost:3000/'
 }
