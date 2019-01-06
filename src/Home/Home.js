d818b23be92f8183d44e4937004048163b833e7d
strongSoda <strongsoda2@gmail.com>
2019-01-06 16:51:12 +0530

    switching back to localStorage

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,26 +2,8 @@ import React, { Component } from 'react';
 import Todo from '.././Components/Todo';
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
-import Forage from 'react-localforage'
 
-
-const ACCESS_TOKEN =   <Forage.GetItem
-                          key='access_token'
-                          render={({inProgress, value, error}) => {
-                            return (
-                              <div>
-                                {error &&
-                                <div>
-                                  {error.message}
-                                </div>}
-                                {inProgress && <progress/>}
-                                {value &&
-                                <pre>{JSON.stringify(value, null, 2)}</pre>}
-                              </div>
-                            )
-                          }}
-                        />
-
+const ACCESS_TOKEN = localStorage.getItem('access_token');
 export const client = new ApolloClient({
   uri: "https://hasura-react-todo-apollo.herokuapp.com/v1alpha1/graphql",
   headers: {
