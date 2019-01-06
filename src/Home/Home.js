041f74463981cf82c62803529d7c29ba1b2f51eb
strongSoda <strongsoda2@gmail.com>
2019-01-06 17:49:46 +0530

    code debugging

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,16 +3,20 @@ import Todo from '.././Components/Todo';
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 
-const ACCESS_TOKEN = localStorage.getItem('access_token');
-export const client = new ApolloClient({
-  uri: "https://hasura-react-todo-apollo.herokuapp.com/v1alpha1/graphql",
-  headers: {
-    Authorization: `Bearer ${ACCESS_TOKEN}`,
-  }
-});
-
+export const client;
 
 class Home extends Component {
+
+  constructor(props) {
+    super(props);
+    const ACCESS_TOKEN = localStorage.getItem('access_token');
+    client = new ApolloClient({
+      uri: "https://hasura-react-todo-apollo.herokuapp.com/v1alpha1/graphql",
+      headers: {
+        'Authorization': `Bearer ${ACCESS_TOKEN}`,
+      }
+    });
+  }
   login() {
     this.props.auth.login();
   }
