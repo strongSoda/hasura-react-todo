e7087ea7561366766f7caf39a5f2b99d678ecfc5
strongSoda <strongsoda2@gmail.com>
2019-01-06 19:10:48 +0530

    code debugging

--- src/routes.js
+++ src/routes.js
@@ -18,7 +18,7 @@ export const makeMainRoutes = () => {
   return (
       <Router history={history}>
         <div>
-          <Route path="/" render={(props) => <App {...props} />} />
+          <Route path="/" render={(props) => <App auth={auth} {...props} />} />
           <Route path="/home" render={(props) => <Home auth={auth} {...props} />} />
           <Route path="/callback" render={(props) => {
             handleAuthentication(props);
